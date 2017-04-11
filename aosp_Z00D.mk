abb473fa5e01d742b363ef37eb3551f3d72afd4c
Flex1911 <dedsa2002@gmail.com>
2017-04-11 16:22:23 +0300

    Z00D: bringup device configuration for AOSP

--- aosp_Z00D.mk
+++ aosp_Z00D.mk
@@ -2,18 +2,13 @@
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/asus/Z00D/device.mk)
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_RUNTIMES := runtime_libart_default
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_Z00D
+PRODUCT_NAME := aosp_Z00D
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Z00D
 PRODUCT_MANUFACTURER := asus
